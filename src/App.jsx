726cfbc1faaa6cd9021b4723d8985375862424fc
Miguel Fuentes <mefj6294@hotmail.com>
2026-09-16 20:40:13 -0400

    Render React app entrypoint

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
+import { createRoot } from 'react-dom/client';
 import Dashboard from './Dashboard.jsx';
 import './styles.css';
 
 export default function App() {
   return <Dashboard />;
 }
+
+createRoot(document.getElementById('root')).render(<App />);
